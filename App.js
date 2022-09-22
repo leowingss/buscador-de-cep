6ff5a3a43f28d3fac63c0e0dd0164f8590c7efa0
Leonardo <leondani853@gmail.com>
2022-09-22 10:02:02 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Leo</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
