bf44b2b8a6dd3b6e05ed3cb3600bfd70e8d17894
Leonardo <leondani853@gmail.com>
2022-09-22 10:43:50 -0300

    estilizando app

--- App.js
+++ App.js
@@ -1,16 +1,89 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, SafeAreaView } from "react-native";
+import api from './src/services/api';
 
 export default function App() {
+
+  const [cep, setCep] = useState('');
+
+
   return (
-    <View style={styles.container}>
-      <Text>Leo</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ alignItems: 'center' }}>
+        <Text style={styles.text}>Digite o CEP desejado</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={'Ex: 01001000'}
+          value={cep}
+          onChangeText={(texto) => setCep(texto)}
+          keyboardType="numeric"
+        />
+      </View>
+
+      <View style={styles.areaBtn}>
+        <TouchableOpacity style={[styles.botao, { backgroundColor: '#1d75cd' }]}>
+          <Text style={styles.botaoText}>BUSCAR</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.botao, { backgroundColor: '#cd3e1d' }]}>
+          <Text style={styles.botaoText}>LIMPAR</Text>
+        </TouchableOpacity>
+      </View>
+
+
+      <View style={styles.resultado}>
+        <Text style={styles.itemText}>CEP:</Text>
+        <Text style={styles.itemText}>Logradouro:</Text>
+        <Text style={styles.itemText}>Bairro:</Text>
+        <Text style={styles.itemText}>Cidade:</Text>
+        <Text style={styles.itemText}>Estado:</Text>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  text: {
+    marginTop: 25,
+    marginBottom: 15,
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "#DDD",
+    borderRadius: 5,
+    width: "90%",
+    padding: 10,
+    fontSize: 18
+  },
+  areaBtn: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 15,
+    justifyContent: 'space-around'
+  },
+  botao: {
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    padding: 15,
+  },
+  botaoText: {
+    fontSize: 20,
+    color: '#fff'
+  },
+  resultado: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  itemText: {
+    fontSize: 22
   }
 });
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL:'​https://viacep.com.br/ws'
+});
+
+
+export default api;
